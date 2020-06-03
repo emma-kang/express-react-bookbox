2770836c5e239ed3bd6ce205d7a3fa1c44a888cb
Emma Kang <yuseon1216@gmail.com>
2020-06-02 23:31:54 -0400

    Add footer contents and style

--- client/src/component/scenes/footer/Footer.js
+++ client/src/component/scenes/footer/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './footerStyle.css';
 
 const Footer = (props) => {
     return(
-        <div>
-            Footer
+        <div className="bookbox-footer">
+            <p>@Copyright 2020 All rights reserved.</p>
+            <p>Created by Emma Kang</p>
         </div>
     )
 };
