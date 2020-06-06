cf3c94e371fc382620b4d7eeb5e0faedd976fea2
Emma Kang <yuseon1216@gmail.com>
2020-06-06 01:05:30 -0400

    add redux for book detail page

--- client/src/component/scenes/books/BookList.js
+++ client/src/component/scenes/books/BookList.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import Button from "@material-ui/core/Button";
 import './booksStyle.css';
 
-const BookList = ({ data }) => {
-    return (
-        <div className="book-list">
-            <img src={data.imageurl} alt="book"/>
-            <h3 className="book-list-title">{data.title}</h3>
-            <div className="book-list-info">
-                <p><span className="book-list-info-title">Category: </span>{data.category}</p>
-                <p><span className="book-list-info-title">Publisher: </span>{data.publisher}</p>
-                <p><span className="book-list-info-title">Published: </span>{data.published.split('T')[0]}</p>
-            </div>
-        </div>
-    )
+const BookList = ({data, selectedBook}) => {
+  return (
+    <div className="book-list">
+      <img src={data.imageurl} alt="book"/>
+      <h3 className="book-list-title">{data.title}</h3>
+      <div className="book-list-info">
+        <p><span className="book-list-info-title">Author: </span>{data.author}</p>
+        <Button variant="contained" style={{backgroundColor: '#F1DEB4'}} onClick={() => selectedBook(data)}>See
+          Details</Button>
+      </div>
+    </div>
+  )
 }
 
 export default BookList;
--- client/src/reducer/ducksReducer.js
+++ client/src/reducer/ducksReducer.js
@@ -1,13 +1,23 @@
 import produce from 'immer';
 
-const initState = {
+export const SET_BOOK = "SET_BOOK";
 
+export const redux_set_book = (bookData) => ({
+  type: SET_BOOK, payload: bookData
+});
+
+const initState = {
+  bookData: null
 };
 
 export default function ducksReducer(state = initState, action) {
   return produce(state, (draft) => {
     switch (action.type) {
-
+      case SET_BOOK:
+        draft.bookData = action.payload;
+        break;
+      default:
+        return;
     }
   });
 }
