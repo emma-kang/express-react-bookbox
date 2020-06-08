12bce9d4340ea1f131921bc6a1e248a94ed2beb1
Emma Kang <yuseon1216@gmail.com>
2020-06-07 22:58:19 -0400

    modified user routes

--- api/server/routes/users.js
+++ api/server/routes/users.js
@@ -1,11 +1,10 @@
 import express from 'express';
 
 import { loginUser, createNewUser } from '../controllers/userController';
-import verifyAuth from '../middleware/verifyAuth';
 
 const router = express.Router();
 
-router.get('/login', verifyAuth, loginUser);
-router.post('/signup', verifyAuth, createNewUser);
+router.post('/login', loginUser);
+router.post('/signup', createNewUser);
 
 module.exports = router;
