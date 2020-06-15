85bd64f44200224afb7cc34974b4e7cc14c123f0
Emma Kang <yuseon1216@gmail.com>
2020-06-14 21:01:55 -0400

    modified books route and controller

--- server/routes/books.js
+++ server/routes/books.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import { getAllBooks } from '../controllers/bookController';
+import { getAllBooks, getBookById, updateBook, deleteBook } from '../controllers/bookController';
 
 const router = express.Router();
 
 router.get('/', getAllBooks);
+router.get('/:bookId', getBookById);
+router.post('/:bookId', updateBook);
+router.delete('/:bookId', deleteBook);
 
 module.exports = router;
