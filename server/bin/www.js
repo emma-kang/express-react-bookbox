0b89c0037fc14a19b5c92cc5d1336537905668cb
Emma Kang <yuseon1216@gmail.com>
2020-06-18 23:44:06 -0400

    fix module import

--- server/bin/www.js
+++ server/bin/www.js
@@ -4,9 +4,9 @@
  * Module dependencies.
  */
 
-var app = require('../app');
-var debug = require('debug')('bookbox-api:server');
-var http = require('http');
+import app from '../app';
+import debug from 'debug';
+import http from 'http';
 
 /**
  * Get port from environment and store in Express.
