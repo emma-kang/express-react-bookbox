c04087d43a3b950bc642dfd9d4ac233bc7394e8b
Emma Kang <yuseon1216@gmail.com>
2020-06-28 21:14:25 -0400

    restructure

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -3,6 +3,11 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: true
+    }
+});
 
 module.exports = sequelize;
