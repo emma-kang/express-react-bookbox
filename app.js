28bc2cef9e005021ba391a96a55eb3f3e947460b
Emma Kang <yuseon1216@gmail.com>
2020-06-28 21:37:03 -0400

    add routes

--- app.js
+++ app.js
@@ -10,9 +10,10 @@ const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const swaggerDocument = YAML.load('swagger.yaml');
 
-// const usersRouter = require('./routes/users');
-// const bookRouter = require('./routes/books');
-const commentRouter = require('./routes/comments');
+const usersRouter = require('./routes/users');
+const booksRouter = require('./routes/books');
+const commentsRouter = require('./routes/comments');
+const adminRouter = require('./routes/admin');
 
 const app = express();
 
@@ -20,7 +21,7 @@ app.use(cors());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // view engine setup
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
@@ -28,9 +29,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// app.use('/api/v1/users', usersRouter);
-// app.use('/api/v1/books', bookRouter);
-app.use('/api/v1/comments', commentRouter);
+app.use('/api/v1/users', usersRouter);
+app.use('/api/v1/books', booksRouter);
+app.use('/api/v1/comments', commentsRouter);
+app.use('/api/v1/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
