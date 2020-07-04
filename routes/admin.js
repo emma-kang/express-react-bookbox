a24becc1047ee9261d345af658af713e203ceda1
Emma Kang <yuseon1216@gmail.com>
2020-07-04 19:14:13 -0400

    added admin route

--- routes/admin.js
+++ routes/admin.js
@@ -7,6 +7,12 @@ const commentsController = require('../controllers/commentController');
 const router = express.Router();
 
 router.get('/authors', authorController.getAuthors);
+router.post('/authors/:authorid', authorController.updateAuthor);
+router.delete('/authors/:authorid', authorController.deleteAuthor);
+
+router.get('/books', booksController.getBooks);
+router.patch('/books/:bookid', booksController.updateBook);
+router.delete('/books/:bookid', booksController.deleteBook);
 
 module.exports = router;
 
